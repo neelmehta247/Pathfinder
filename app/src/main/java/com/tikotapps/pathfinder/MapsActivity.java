826caad09f11c6c9f1781f248ac0c86c08c75e22
Neel Mehta <neelmehta247@gmail.com>
2015-09-03 09:02:17 +0530

    ListView added. Functionality to FAB added.

--- app/src/main/java/com/tikotapps/pathfinder/MapsActivity.java
+++ app/src/main/java/com/tikotapps/pathfinder/MapsActivity.java
@@ -6,22 +6,35 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.software.shell.fab.ActionButton;
 
 public class MapsActivity extends AppCompatActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private ActionButton showListButton;
+    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        showListButton = (ActionButton) findViewById(R.id.showList);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+        showListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.setDisplayedChild(Math.abs(viewFlipper.getDisplayedChild() - 1));
+            }
+        });
     }
 
     @Override
@@ -34,8 +47,7 @@ public class MapsActivity extends AppCompatActivity {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
